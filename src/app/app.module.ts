6ad36e0f4796f200e7dfc1cdcda5939f0f9f4afe
Erick-Pereira <erickbaptistapereira@gmail.com>
2025-03-29 03:56:53 -0300

    ci(ajuste no ci): ajuste no CI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,22 @@
-  import { NgModule } from '@angular/core';
-  import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-  import { AppRoutingModule } from './app-routing.module';
-  import { AppComponent } from './app.component';
-  import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-  import { ToastrModule } from 'ngx-toastr';
-  import { SharedModule } from './shared/shared.module';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+import { SharedModule } from './shared/shared.module';
 
-  @NgModule({
-    declarations: [AppComponent],
-    imports: [
-      BrowserModule,
-      ToastrModule.forRoot(),
-      SharedModule,
-      AppRoutingModule,
-      BrowserAnimationsModule,
-    ],
-    providers: [],
-    bootstrap: [AppComponent],
-  })
-  export class AppModule {}
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    ToastrModule.forRoot(),
+    SharedModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
